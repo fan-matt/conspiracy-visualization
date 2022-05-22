cce441e8cee6a3b14fd6e05017dc491a80a78f5c
jshen12 <35182823+jshen12@users.noreply.github.com>
2022-05-22 00:59:52 -0700

    Fixed type error, plus move around stuff

--- client/src/components/PopupGraph.tsx
+++ client/src/components/PopupGraph.tsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 let test = {keyword: "trump", startdate: "2021-01-18", enddate: "2021-02-20"};
 const graphTerms: string[] = ["parler", "trump", "jfk", "moon landing", "russia"];
 
-//Function for getting a string in YYYY-MM-DD format
-function dateToString(date: Date){
-  let mm = date.getMonth()+1;
-  let dd = date.getDate();
-  let dateParts = [date.getFullYear(), (mm>9 ? "" : "0") + mm,(dd>9 ? "" : "0") + dd];
-  return dateParts.join("-");
-}
 
-type dateFreq = {
-  date: Date,
-  frequency: number,
-};
+type dateFreq = [date: Date, num: number];
+
 
 type Props = {
   terms: string[];
@@ -23,14 +14,11 @@ type Props = {
 
 const PopupGraph = ({ terms }: Props) =>{
   const [ graphData , setGraphData] = useState<Array<dateFreq>>([]);
+  //const [ searchTerm, setSearchTerm] = useState("trump");
   // Getting the start and end date in string form
-  let currDate: Date = new Date("2021-02-20");
-  let endDate: string = dateToString(currDate)
-  currDate.setDate(currDate.getDate()-30);
-  let startDate: string = dateToString(currDate);
-  function fetchData(term: string) {
-    console.log("Fetching Data!")
-    let currInput = {keyword: term, startdate: startDate, enddate: endDate}
+  
+  function fetchTimeSeries() {
+    let currInput = {keyword: "trump", startdate: "2021-01-18", enddate: "2021-02-20"};
     fetch("./api/getTimeSeries", {
       method: "POST",
       headers: {
@@ -39,12 +27,36 @@ const PopupGraph = ({ terms }: Props) =>{
       body: JSON.stringify({ input: currInput }),
     })
     .then((response) => response.json())
-    .then((response) => {setGraphData(response); console.log(response); console.log("We got the data!")});
-    console.log("Finished fetching data!");
+    .then((response) => {console.log(response); setGraphData(response); console.log(graphData)});
   }
-  fetchData("trump");
+
+  useEffect(() => {
+    //let currDate: Date = new Date("2021-02-20");
+    //let endDate: string = dateToString(currDate);
+    //currDate.setDate(currDate.getDate()-30);
+    //let startDate: string = dateToString(currDate);
+    
+    console.log("fetching timeseries data...");
+    
+    
+    fetchTimeSeries();
+  }, []);
+
+  //Function for getting a string in YYYY-MM-DD format
+  const dateToString = graphData.map((dateFreq) => {
+    let thisDate = new Date(dateFreq[0]);
+    let mm = thisDate.getMonth()+1;
+    let dd = thisDate.getDate();
+    let dates = [thisDate.getFullYear(), (mm>9 ? "" : "0") + mm,(dd>9 ? "" : "0") + dd].join("-");
+
+    return(
+      <p>{dates} {dateFreq[1]}</p>
+    );
+  })
+
+
   return(
-    <div>{graphData.map((dateFreq) => {return <p>{dateToString(dateFreq.date)} {dateFreq.frequency}</p>})}</div>
+    <div>{dateToString}</div>
   );
 }
 
